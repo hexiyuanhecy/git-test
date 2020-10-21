cb6130a8dc1234115652c658d71e893d92edcfa8
hexiyuan <hexiyuanhecy@gmial.com>
2020-10-21 11:00:02 +0800

    I wanna leave!

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  * @Author: hexy
  * @Date: 2020-10-21 10:56:09
  * @LastEditors: hexy
- * @LastEditTime: 2020-10-21 10:57:26
+ * @LastEditTime: 2020-10-21 10:59:37
  * @FilePath: \git\src\main.js
  */
-console.log('I wanna leave as soon as possible!')
+console.log('I wanna leave!')
