1e147ee0e42d8ac31c5706285d13f56639f89cb0
hexiyuan <hexiyuanhecy@gmial.com>
2020-10-21 10:58:17 +0800

    I wanna leave as soon as possible!

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @Author: hexy
+ * @Date: 2020-10-21 10:56:09
+ * @LastEditors: hexy
+ * @LastEditTime: 2020-10-21 10:57:26
+ * @FilePath: \git\src\main.js
+ */
+console.log('I wanna leave as soon as possible!')
