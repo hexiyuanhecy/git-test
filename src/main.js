bd82ee185d69bfd763d20c8f844aa9f388b8cf1c
hexiyuan <hexiyuanhecy@gmial.com>
2020-10-22 18:47:38 +0800

    版本1

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
  * @Author: hexy
  * @Date: 2020-10-21 10:56:09
  * @LastEditors: hexy
- * @LastEditTime: 2020-10-21 10:59:37
+ * @LastEditTime: 2020-10-22 18:47:29
  * @FilePath: \git\src\main.js
  */
 console.log('I wanna leave!')
+console.log('版本1')
