a1d34371f34f76850063bf5d5e185fcfbf64c56d
hexiyuan <hexiyuanhecy@gmial.com>
2020-10-22 19:48:28 +0800

    版本4‘

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@
  * @Author: hexy
  * @Date: 2020-10-21 10:56:09
  * @LastEditors: hexy
- * @LastEditTime: 2020-10-22 18:48:08
+ * @LastEditTime: 2020-10-22 19:42:31
  * @FilePath: \git\src\main.js
  */
 console.log('I wanna leave!')
 console.log('版本1')
 console.log('版本2')
 console.log('版本3')
+console.log('版本4')
---
+++ src/test.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @Author: hexy
+ * @Date: 2020-10-22 19:30:53
+ * @LastEditors: hexy
+ * @LastEditTime: 2020-10-22 19:30:56
+ * @FilePath: \git\src\test.js
+ */
+123
